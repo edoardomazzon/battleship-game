594ba13eec3d9db0b6b812820ca48d19d6f5dd0f
CeccoGab <48321569+CeccoGab@users.noreply.github.com>
2022-08-03 17:07:43 +0200

    Added comments ENG to friendrequestservice

--- client/src/app/my-profile/my-profile.component.ts
+++ client/src/app/my-profile/my-profile.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs';
 import { ProfileService } from '../services/profile.service';
 import { User } from '../models/user';
@@ -18,8 +17,7 @@ export class MyProfileComponent implements OnInit {
   public blacklisted_users: Array<String> = [];
   public friends: Array<String> = [];
 
-  constructor(private _httpClient: HttpClient,
-              private _profileService: ProfileService,
+  constructor(private _profileService: ProfileService,
               private _friendRequestService: FriendRequestService) { }
 
   ngOnInit(): void {
@@ -29,36 +27,48 @@ export class MyProfileComponent implements OnInit {
 
       this.getUserInfo((JSON.parse(this.current_user)).username)//Aggiorniamo il localstorage con le nuove info dell'utente dal db
 
+      // This function is invoked by the friend-request service, which whill return an observable for Socket.io emits.
+      // Once the service listens to an emit from the server, the client's component is notified and is engaged differently
+      // depending on the request type notified by the server.
+      // I.E. the "observer" object returned by this function, which has been hit by a next() call, carries within itself some information:
+      // who the request sender is, the request receiver and the request_type (someone accepted us, we accepted somebody's request, we rejected someone's request,
+      // etc.
       this._friendRequestService.listenToAnsweredRequests(((JSON.parse(this.current_user)).username)).subscribe((observer)=>{
-        if(observer.request_type == 'friendrequest'){
-          var current = localStorage.getItem('current_user') //Magari conviene unire questa request_type a quella sotto di reject così risparmiamo codice
-          if(current != null){
-            this.friend_requests_list = (JSON.parse(JSON.parse(JSON.stringify(current)))).pending_friend_requests
-          }
-        }
-        if(observer.request_type == 'reject'){
+        
+        //If we reject someone's request, or if we receive someone's request, we still have to update the component's istance's friend requests list in both cases.
+        if(observer.request_type == 'reject' || observer.request_type = 'friendrequest'){
           var current = localStorage.getItem('current_user')
           if(current != null){
             this.friend_requests_list = (JSON.parse(JSON.parse(JSON.stringify(current)))).pending_friend_requests
           }
         }
+        
+        //If we blacklisted a user, we have to udpate the component's istances's blacklisted_users list with that new user in it.
+        //Notice how we don't update the pending_friend_requests list: that's because the blacklistUser() function immediately invokes the rejectFriendRequest()
+        //function, which will update the new pending_friend_requests by itself before reaching this point of the execution.
         else if(observer.request_type == 'block'){
           var current = localStorage.getItem('current_user')
           if(current != null){
-            //Qui non aggiorno anche la pending requests list perché ci ha già pensato la rejectFriendRequest() chiamata subito dalla blacklistUser()
             this.blacklisted_users = (JSON.parse(JSON.parse(JSON.stringify(current)))).blacklisted_users
           }
         }
+        
+        // When we accept someone's request, we have to update our friends list in the component's istance.
+        // As for updating the pending requests list, the acceptFriendRequest() counterintuitively invokes rejectFriendRequest(), since its only
+        // function is that of deleting the rejected (in this case accepted) user from the pending requests list. That's beacuse, wether we accept 
+        // or reject someone's request, we still have to pop that someone's username out of our pending requests list, since the request is not pending anymore.
         else if(observer.request_type = 'accept'){ //Io ho accettato la richiesta
           var current = localStorage.getItem('current_user')
           if(current != null){
-            this.friends = new Array<String>()
             var newlist = (JSON.parse(current)).friends_list
             for(let i = 0; i < newlist.length; i++){
               this.friends.push(newlist[i])
             }
           }
         }
+        
+        // A 'yougotaccepted'+current_username emit is sent from the server when our friend request to some other user gets accepted, so the service
+        // catches it and notifies the component through the observer. We then update our friends list and pending friend requests list.
         else if(observer.request_type = 'yougotaccepted'){ //Qualcuno ha accettato la mia richiesta
           console.log('L\'utente '+observer.accepting_user+' ha accettato la nostra richiesta di amicizia')
           var current = localStorage.getItem('current_user')
@@ -68,7 +78,7 @@ export class MyProfileComponent implements OnInit {
             newcurrent.friends.push(observer.accepting_user)
             localStorage.removeItem('current_user')
             localStorage.setItem('current_user', JSON.stringify(newcurrent))
-
+            //manca l'aggiornamento della pending list di noi che siamo stati accettati
             this.friends = newcurrent.friends
           }
         }
@@ -129,4 +139,4 @@ export class MyProfileComponent implements OnInit {
     blacklist_request.sender = JSON.parse(JSON.parse(JSON.stringify((localStorage.getItem('current_user'))))).username
     this._friendRequestService.blacklistUser(blacklist_request)
   }
-}
+  
