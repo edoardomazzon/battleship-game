fa224fa92a1158339afca09dcd28e5c409bf4965
CeccoGab <lele.cec98@gmail.com>
2022-08-14 13:00:27 +0200

    Starting chat when a game starts

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatmessageService } from '../services/chatmessage.service';
 
 @Component({
   selector: 'app-game',
@@ -8,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class GameComponent implements OnInit {
   public current_user: any
   public enemy: any
-  constructor() {
+  public myfield: any // Matrix containing the user's ships' positions
+  public enemyfield: any // Matrix containing enemy's ships' positions
+
+  constructor(private _chatMessageService: ChatmessageService) {
     this.current_user = JSON.parse(JSON.parse(JSON.stringify(localStorage.getItem('current_user'))))
     this.enemy = localStorage.getItem('matchinfo')
     if(this.enemy){
       this.enemy = JSON.parse(this.enemy).enemy
     }
+    this._chatMessageService.startChat({
+      current_user: this.current_user.username,
+      other_user: this.enemy
+    })
   }
 
   ngOnInit(): void {
